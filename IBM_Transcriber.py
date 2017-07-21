78dec0e69db4a8fec461277c1ea21830fef9d39f
Kassmanben <Kassmanben@gmail.com>
2017-07-21 13:18:12 -0400

    Added comments to IBM_Transcriber
    ^^^^^^

--- IBM_Transcriber.py
+++ IBM_Transcriber.py
@@ -3,6 +3,9 @@ import pickle
 from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
 
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+# Get these from the Blumix service page. There are not your Bluemix username and password
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
 speech_to_text = SpeechToTextV1(
     username='7f202b76-1946-4379-b826-63c9d775908d',
     password='6AY6L7ah4Npy',
@@ -13,13 +16,24 @@ print(json.dumps(speech_to_text.models(), indent=2))
 
 print(json.dumps(speech_to_text.get_model('en-US_BroadbandModel'), indent=2))
 
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+# Enter this in.
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
 trial_number = '9'
 trial_date = '07_07_17'
 
 
 input_file = "Trial_"+trial_number+"_"+trial_date+'/Trial_'+trial_number+"_"+trial_date+'.flac'
 output_file = "Trial_"+trial_number+"_"+trial_date+'/Trial_'+trial_number+'_IBM_Transcript.json'
-#
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+# Sends the .flac file to IBM Watson
+# NOTE: The .flac file must be under 100 MB. I use ffmpeg to convert the .mov file to .flac
+# then use Audacity to compress the .flac file to under 100 MB
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 with open(join(dirname(__file__), input_file), 'rb') as audio_file:
     data = speech_to_text.recognize(
         audio_file, content_type='audio/flac', timestamps=True,
@@ -35,6 +49,10 @@ with open(output_file, 'r') as handle:
 print(json.dumps(data["speaker_labels"], indent=2))
 print(json.dumps(data["results"], indent=2))
 
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+# Creates a list with each line of the transcript represented in a list
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
 transcript_list = []
 for a in data["results"]:
     word_confidence = a['alternatives'][0]['word_confidence']
@@ -42,6 +60,9 @@ for a in data["results"]:
     transcript = a['alternatives'][0]['transcript']
     transcript_list.append([word_confidence, timestamps, transcript])
 
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+# Speaker data is kept in another list with timestamps
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
 speaker_data = []
 for s in data["speaker_labels"]:
     speaker = s["speaker"]
